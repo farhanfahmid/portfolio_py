3fe2e5dc63d4259a26e750d0a1c5e912e72f9994
farhanfahmid <fahmidfarhan274@gmail.com>
2025-03-16 22:21:58 +0600

    Added better formatted data file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 # st.set_page_config(layout="wide")
 
@@ -10,6 +11,28 @@ with col1:
 with col2:
     st.title("Farhan Fahmid")
     content = """
-    Hey, I'm Farhan Fahmid! I am a Python Programmer, MBA student, and CSE graduate. Check out my Python projects below1
+    Hey, I'm Farhan Fahmid! I am an aspiring Python Programmer, MBA student, and CSE graduate. I'm continuosly learning and improving my programming skills by building projects.
     """
     st.info(content)
+
+st.info("Check out some of the Python projects I have worked on. And feel free to contact me anytime!")
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5]) #ratio of column widths
+
+df = pd.read_csv(r"G:\PYTHON PROJECTS\Portfolio Website\data2.csv", encoding='ISO-8859-1') #sep tells that the data are separated by ;
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.write(f"[Source Code]({row['code']})")
+        st.write(f"[Check it Out]({row['demo']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.write(f"[Source Code]({row['code']})")
+        st.write(f"[Check it Out]({row['demo']})")
